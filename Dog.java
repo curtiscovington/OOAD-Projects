1fb536e3daf0928020bb87c8c5ab3eb4a3da158b
Curtis Covington <curtis.covington@gmail.com>
2022-06-08 14:06:21 -0600

    init Dog

---
+++ Dog.java
@@ -0,0 +1,46 @@
+public class Dog extends Pet {
+    private int size;
+    private String color;
+    private boolean housebroken;
+    private boolean purebred;
+
+    public Dog(String name, double purchasePrice, double listPrice, double salePrice, int dayArrived, int daySold, String breed, int age, boolean healthy, int size, String color, boolean housebroken, boolean purebred) {
+        super(name, purchasePrice, listPrice, salePrice, dayArrived, daySold, breed, age, healthy);
+        this.size = size;
+        this.color = color;
+        this.housebroken = housebroken;
+        this.purebred = purebred;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isHousebroken() {
+        return housebroken;
+    }
+
+    public boolean isPurebred() {
+        return purebred;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setHousebroken(boolean housebroken) {
+        this.housebroken = housebroken;
+    }
+
+    public void setPurebred(boolean purebred) {
+        this.purebred = purebred;
+    }
+}
