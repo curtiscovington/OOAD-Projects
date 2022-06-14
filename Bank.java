5678737a1e043b45a50f093aa988a6d52a9a6848
Curtis Covington <curtis.covington@gmail.com>
2022-06-14 17:46:36 -0600

    get amount withdrawn

--- Bank.java
+++ Bank.java
@@ -8,4 +8,8 @@ public class Bank {
         this.amountWithdrawn += requestedAmount;
         return requestedAmount;
     }
+
+    public double getAmountWithdrawn() {
+        return this.amountWithdrawn;
+    }
 }
